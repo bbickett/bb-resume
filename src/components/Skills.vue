e04438d1a794dea46f2ea2264894980ebe1ed36d
Brian Bickett <brian@bickett.dev>
2021-08-16 00:05:39 -0400

    finish adding skills and hobby sections

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -2,18 +2,129 @@
     <div class="skills-section">
         <h4 class="page-heading">Skills</h4>
 
-        <h5 class="category">Languages</h5>
-        <ul class="skills">
-            <li>JavaScript</li>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>C#</li>
-            <li>Python</li>
-            <li>SCSS</li>
-            <li>SQL</li>
-            <li>Java</li>
-            <li>PHP</li>
-        </ul>
+
+        <h5 class="header">Languages</h5>
+        <div class="skills">
+            <div class="skill">JavaScript
+                <b-progress value="90" height="5px"></b-progress>
+            </div>
+            <div class="skill">HTML
+                <b-progress value="96" height="5px"></b-progress>
+            </div>
+            <div class="skill">CSS
+                <b-progress value="96" height="5px"></b-progress>
+            </div>
+            <div class="skill">C#
+                <b-progress value="83" height="5px"></b-progress>
+            </div>
+            <div class="skill">Python
+                <b-progress value="66" height="5px"></b-progress>
+            </div>
+            <div class="skill">SCSS
+                <b-progress value="85" height="5px"></b-progress>
+            </div>
+            <div class="skill">SQL
+                <b-progress value="82" height="5px"></b-progress>
+            </div>
+            <div class="skill">Java
+                <b-progress value="70" height="5px"></b-progress>
+            </div>
+            <div class="skill">PHP
+                <b-progress value="60" height="5px"></b-progress>
+            </div>
+        </div>
+
+
+        <h5 class="header">Libraries / Frameworks</h5>
+        <div class="skills">
+            <div class="skill">Vue.js
+                <b-progress value="91" height="5px"></b-progress>
+            </div>
+            <div class="skill">Bootstrap
+                <b-progress value="96" height="5px"></b-progress>
+            </div>
+            <div class="skill">Webpack
+                <b-progress value="95" height="5px"></b-progress>
+            </div>
+            <div class="skill">.NET MVC Core
+                <b-progress value="93" height="5px"></b-progress>
+            </div>
+            <div class="skill">Entity Framework
+                <b-progress value="93" height="5px"></b-progress>
+            </div>
+            <div class="skill">Babel
+                <b-progress value="90" height="5px"></b-progress>
+            </div>
+            <div class="skill">Web Forms
+                <b-progress value="95" height="5px"></b-progress>
+            </div>
+            <div class="skill">TensorFlow
+                <b-progress value="70" height="5px"></b-progress>
+            </div>
+            <div class="skill">Django
+                <b-progress value="60" height="5px"></b-progress>
+            </div>
+        </div>
+
+
+
+
+        <h5 class="header">Development Tools</h5>
+        <div class="skills">
+            <div class="skill">VS Code
+                <b-progress value="85" height="5px"></b-progress>
+            </div>
+            <div class="skill">Visual Studio
+                <b-progress value="80" height="5px"></b-progress>
+            </div>
+            <div class="skill">Chrome DevTools
+                <b-progress value="95" height="5px"></b-progress>
+            </div>
+            <div class="skill">GitHub
+                <b-progress value="90" height="5px"></b-progress>
+            </div>
+            <div class="skill">GitLab
+                <b-progress value="85" height="5px"></b-progress>
+            </div>
+            <div class="skill">Netlify
+                <b-progress value="80" height="5px"></b-progress>
+            </div>
+            <div class="skill">Node
+                <b-progress value="85" height="5px"></b-progress>
+            </div>
+            <div class="skill">Jupyter Notebooks
+                <b-progress value="60" height="5px"></b-progress>
+            </div>
+            <div class="skill">SQL Server Management Studio
+                <b-progress value="90" height="5px"></b-progress>
+            </div>
+        </div>
+
+
+
+        <h5 class="header">Personal</h5>
+        <div class="skills">
+            <div class="skill">Problem Solver
+                <b-progress value="95" height="5px"></b-progress>
+            </div>
+            <div class="skill">Critical Thinker
+                <b-progress value="90" height="5px"></b-progress>
+            </div>
+            <div class="skill">Detail Oriented
+                <b-progress value="90" height="5px"></b-progress>
+            </div>
+            <div class="skill">Team Leader
+                <b-progress value="85" height="5px"></b-progress>
+            </div>
+            <div class="skill">Self-Motivated
+                <b-progress value="85" height="5px"></b-progress>
+            </div>
+            <div class="skill">Responsible
+                <b-progress value="95" height="5px"></b-progress>
+            </div>
+        </div>
+
+
     </div>
 </template>
 
